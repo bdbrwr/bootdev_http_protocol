c6367acd22a2a97f036b757636652b5aee424600
bdbrwr <bart@bdbrwr.nl>
2025-07-18 23:45:47 +0200

    File read into 8 byte slice

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	f, err := os.Open("messages.txt")
+	if err != nil {
+		log.Fatal("error", "error", err)
+	}
+	defer f.Close()
+
+	for {
+		data := make([]byte, 8)
+		n, err := f.Read(data)
+		if err != nil {
+			break
+		}
+
+		fmt.Printf("read: %s\n", string(data[:n]))
+	}
 }
