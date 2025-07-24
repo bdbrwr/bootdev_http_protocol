a4fcfb3766b35fe27d8c5e5d9190e7b92c640950
bdbrwr <bart@bdbrwr.nl>
2025-07-24 17:06:47 +0200

    Video

--- cmd/httpserver/main.go
+++ cmd/httpserver/main.go
@@ -76,6 +76,16 @@ func main() {
 			body = respond500()
 			status = response.StatusInternalServerError
 
+		} else if req.RequestLine.RequestTarget == "/video" {
+			f, _ := os.ReadFile("assets/vim.mp4")
+			h.Replace("Content-Type", "video/mp4")
+			h.Replace("Content-Length", fmt.Sprintf("%d", len(f)))
+
+			w.WriteStatusLine(response.StatusOK)
+			w.WriteHeaders(*h)
+			w.WriteBody(f)
+			return
+
 		} else if strings.HasPrefix(req.RequestLine.RequestTarget, "/httpbin/") {
 			target := req.RequestLine.RequestTarget
 			res, err := http.Get("https://httpbin.org/" + target[len("/httpbin/"):])
